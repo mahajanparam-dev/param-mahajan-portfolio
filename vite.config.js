01d6531a003a1c0ca3059a77f84a007cf133d0e8
Param Mahajan <mahajanparam361@gmail.com>
2026-02-20 22:06:13 +0530

    Clean production-ready portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/param-portfolio/", // <-- set this to your repo name
+  base: "/param-mahajan-portfolio/", // <-- set this to your repo name
   plugins: [react()],
 });
