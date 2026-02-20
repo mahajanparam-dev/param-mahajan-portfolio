8f2c3746c0ee9996026f0e93709e01b35bf57410
Param Mahajan <mahajanparam361@gmail.com>
2026-02-20 22:13:21 +0530

    Add docs build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/param-mahajan-portfolio/", // <-- set this to your repo name
+  build: {
+    outDir: "docs"
+  },
   plugins: [react()],
 });
